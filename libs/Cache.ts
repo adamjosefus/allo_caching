9ef67da8512989c2dc846873301684001cb5ecca
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-27 19:04:37 +0100

    Update

--- libs/Cache.ts
+++ libs/Cache.ts
@@ -2,16 +2,28 @@
  * @copyright Copyright (c) 2022 Adam Josefus
  */
 
+import { type DependenciesType } from "./DependenciesType.ts";
 
 export type GeneratorType<T> = () => T;
-type LoadOnlyEntryType = [key: string];
-type LoadAndGenerateEntryType<T> = [key: string, generator: GeneratorType<T>];
-type LoadEntryType<T> = LoadOnlyEntryType | LoadAndGenerateEntryType<T>;
+
+type LoadOnlyEntryType =
+    | [key: string]
+    | [key: string, dependencies: DependenciesType];
+
+type LoadAndGenerateEntryType<T> =
+    | [key: string, generator: GeneratorType<T>]
+    | [key: string, generator: GeneratorType<T>, dependencies?: DependenciesType];
+
+type LoadEntryType<T> =
+    | LoadOnlyEntryType | LoadAndGenerateEntryType<T>;
 
 
 export class Cache<T> {
 
-    readonly #storage: Map<string, T> = new Map();
+    readonly #storage: Map<string, {
+        value: T,
+        dependencies?: DependenciesType,
+    }> = new Map();
 
 
     /**
@@ -20,15 +32,46 @@ export class Cache<T> {
      * @returns `<T>` or `<T> | undefined` – depending on whether the generator has been set.
      */
     load<E extends LoadEntryType<T>>(...args: E): E extends LoadAndGenerateEntryType<T> ? T : T | undefined {
-        const [key, generator] = args;
+        const { key, generator, dependencies } = (() => {
+            const [key, a, b] = args;
 
-        if (this.#storage.has(key)) {
-            return this.#storage.get(key)!;
+            if (key !== undefined && a !== undefined && b !== undefined) {
+                return {
+                    key,
+                    generator: a as GeneratorType<T>,
+                    dependencies: b as DependenciesType,
+                }
+
+            } else if (typeof a === 'function') {
+                return {
+                    key,
+                    generator: a as GeneratorType<T>,
+                    dependencies: undefined,
+                }
+
+            } else if (typeof a === 'object') {
+                return {
+                    key,
+                    generator: undefined,
+                    dependencies: a as DependenciesType,
+                }
+
+            } else {
+                return {
+                    key,
+                    generator: undefined,
+                    dependencies: undefined,
+                }
+            }
+        })();
+
+        if (this.has(key)) {
+            return this.#load(key)!;
         }
 
         if (generator) {
             const value = generator();
-            this.save(key, value);
+            this.save(key, value, dependencies);
 
             return value;
         }
@@ -38,13 +81,32 @@ export class Cache<T> {
     }
 
 
+    #load(key: string): T | undefined {
+        const stored = this.#storage.get(key);
+        if (stored === undefined) return undefined;
+
+        const { value, dependencies } = stored;
+        if (dependencies === undefined) return value;
+
+        if (!this.#isDependenciesValid(dependencies)) return undefined;
+        return value;
+    }
+
+
+    #isDependenciesValid(dependencies: DependenciesType): boolean {
+    }
+
+
     /**
      * Save value to cache by key.
      * @param key 
      * @param value 
      */
-    save(key: string, value: T): void {
-        this.#storage.set(key, value);
+    save(key: string, value: T, dependencies?: DependenciesType): void {
+        this.#storage.set(key, {
+            value,
+            dependencies,
+        });
     }
 
 
@@ -54,15 +116,26 @@ export class Cache<T> {
      * @param value 
      */
     has(key: string): boolean {
-        return this.#storage.has(key);
+        return this.#load(key) !== undefined;
     }
 
 
     /**
+     * Remove value from cache.
+     * @param key 
+     */
+    remove(key: string): void {
+        this.#storage.delete(key);
+    }
+
+
+    /**
+     * @deprecated Use `remove` instead.
+     * 
      * Delete value from cache.
      * @param key 
      */
     delete(key: string): void {
-        this.#storage.delete(key);
+        this.remove(key);
     }
 }
