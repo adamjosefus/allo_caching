12ad3eed3d80190ef88d642636fe243f75b4e32c
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-03-05 22:20:25 +0100

    Update Cache.test.ts

--- tests/Cache.test.ts
+++ tests/Cache.test.ts
@@ -2,19 +2,20 @@ import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { Cache, DependenciesType } from "../mod.ts";
 
 
-Deno.test("Cache: save & load", () => {
-    const set = [
-        { key: "undefined", value: undefined },
-        { key: "null", value: null },
-        { key: "number", value: 123 },
-        { key: "string", value: "Lorem ipsum" },
-        { key: "array", value: [10, 20, 30] },
-        { key: "object", value: { foo: "bar" } },
-    ];
+const testSet = [
+    { key: "undefined", value: undefined },
+    { key: "null", value: null },
+    { key: "number", value: 123 },
+    { key: "string", value: "Lorem ipsum" },
+    { key: "array", value: [10, 20, 30] },
+    { key: "object", value: { foo: "bar" } },
+];
 
+
+Deno.test("Cache: save & load", () => {
     const cache = new Cache<unknown>();
 
-    set.forEach(({ key, value }) => {
+    testSet.forEach(({ key, value }) => {
         cache.save(key, value);
         assertEquals(cache.load(key), value);
     });
@@ -22,60 +23,35 @@ Deno.test("Cache: save & load", () => {
 
 
 Deno.test("Cache: load & generator", () => {
-    const set = [
-        { key: "undefined", value: undefined },
-        { key: "null", value: null },
-        { key: "number", value: 123 },
-        { key: "string", value: "Lorem ipsum" },
-        { key: "array", value: [10, 20, 30] },
-        { key: "object", value: { foo: "bar" } },
-    ];
-
     const cache = new Cache<unknown>();
 
-    set.forEach(({ key, value }) => {
+    testSet.forEach(({ key, value }) => {
         assertEquals(cache.load(`${key}_1`, () => value), value);
         assertEquals(cache.load(`${key}_2`), undefined);
     });
 });
 
 
-async function sleep(time: number) {
-    return await new Promise((resolve) => setTimeout(resolve, time));
-}
+Deno.test({
+    name: "Cache: save & dependencies (expire)",
+    fn: () => new Promise((exit) => {
+        const expiration = 100;
+        const dependencies: DependenciesType = { expire: expiration };
+        
+        const cache = new Cache<unknown>();
 
-Deno.test("Cache: save & dependencies (expire)", async () => {
-    const expire = 100;
-    const dependencies: DependenciesType = { expire };
+        const promises: Promise<void>[] = testSet.map(({ key, value }) => new Promise((exit) => {
+            cache.save(key, value, dependencies);
 
-    const key = "key";
-    const cache = new Cache<unknown>();
-    const value = "abc123";
+            assertEquals(cache.load(key), value);
 
-    cache.save(key, value, dependencies);
-
-    await sleep(expire * .51);
-    assertEquals(cache.load(key), value);
-
-    await sleep(expire * .51);
-    assertEquals(cache.load(key), undefined);
-});
+            setTimeout(() => {
+                assertEquals(cache.load(key), undefined);
+                exit();
+            }, expiration + 10);
+        }));
 
 
-Deno.test("Cache: save & dependencies (files)", () => {
-    const file = Deno.makeTempFileSync({ prefix: 'my_temp_' });    
-    const dependencies: DependenciesType = {
-        files: file
-    };
-
-    const key = "key";
-    const cache = new Cache<unknown>();
-    const value = "abc123";
-
-    cache.save(key, value, dependencies);
-
-    assertEquals(cache.load(key), value);''
-
-    Deno.removeSync(file);
-    assertEquals(cache.load(key), undefined);
+        Promise.all(promises).then(() => exit());
+    })
 });
