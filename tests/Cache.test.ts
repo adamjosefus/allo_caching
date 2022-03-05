4cf8021076679062ba8d52d70106c37d3401349f
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-03-05 19:04:32 +0100

    Update Cache.test.ts

--- tests/Cache.test.ts
+++ tests/Cache.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
-import { Cache } from "../mod.ts";
+import { Cache, DependenciesType } from "../mod.ts";
 
 
 Deno.test("Cache: save & load", () => {
@@ -37,4 +37,45 @@ Deno.test("Cache: load & generator", () => {
         assertEquals(cache.load(`${key}_1`, () => value), value);
         assertEquals(cache.load(`${key}_2`), undefined);
     });
+});
+
+
+async function sleep(time: number) {
+    return await new Promise((resolve) => setTimeout(resolve, time));
+}
+
+Deno.test("Cache: save & dependencies (expire)", async () => {
+    const expire = 100;
+    const dependencies: DependenciesType = { expire };
+
+    const key = "key";
+    const cache = new Cache<unknown>();
+    const value = "abc123";
+
+    cache.save(key, value, dependencies);
+
+    await sleep(expire * .51);
+    assertEquals(cache.load(key), value);
+
+    await sleep(expire * .51);
+    assertEquals(cache.load(key), undefined);
+});
+
+
+Deno.test("Cache: save & dependencies (files)", () => {
+    const file = Deno.makeTempFileSync({ prefix: 'my_temp_' });    
+    const dependencies: DependenciesType = {
+        files: file
+    };
+
+    const key = "key";
+    const cache = new Cache<unknown>();
+    const value = "abc123";
+
+    cache.save(key, value, dependencies);
+
+    assertEquals(cache.load(key), value);''
+
+    Deno.removeSync(file);
+    assertEquals(cache.load(key), undefined);
 });
